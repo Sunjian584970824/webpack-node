479d749dd0bc5fa371b3d6fbdfc0d54c1b1ea1c2
Sunjian584970824 <584970824@qq.com>
2019-07-03 16:07:27 +0800

    demo

---
+++ demo/src/index.js
@@ -0,0 +1,19 @@
+require('../css/index.css')
+require('../css/a.less')
+import aa from '../img/aa.jpg'
+import bb from '../img/bb.jpg'
+import cc from '../img/cc.png'
+const arr = [1, 2, 3, 4, 5, 0];
+var a = arr.filter(e => e > 0)
+class A {}
+// console.log(ba)
+var aaimages = new Image()
+var bbimages = new Image()
+var ccimages = new Image()
+aaimages.src = aa;
+bbimages.src = bb;
+ccimages.src = cc;
+// document.body.appendChild(aaimages)
+document.body.appendChild(bbimages)
+document.body.appendChild(ccimages)
+console.log(1)
